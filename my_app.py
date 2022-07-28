3a57d56ab5ea48f6f0500f5a9c5bfa3c35f82232
miguel-kjh <miguelangel.kjh@gmail.com>
2022-07-28 08:54:30 +0100

    fixed the problem with hydra framework

--- my_app.py
+++ my_app.py
@@ -2,8 +2,9 @@ from omegaconf import DictConfig, OmegaConf
 import hydra
 
 
-@hydra.main(config_path="conf", config_name="config")
+@hydra.main(config_path="conf", config_name="config", version_base=None)
 def my_app(cfg: DictConfig) -> None:
+    print("Nope, this is not the config you are looking for")
     print(OmegaConf.to_yaml(cfg))
 
 
