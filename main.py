5ccb44729646779ce7367241a28340d1ffafa0a1
miguel-kjh <miguelangel.kjh@gmail.com>
2022-07-28 09:53:12 +0100

    refactor and requirements.txt done!

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import hydra
 
 
 @hydra.main(config_path="conf", config_name="config", version_base=None)
-def my_app(cfg: DictConfig) -> None:
-    print("Nope, this is not the config you are looking for")
+def main(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
 
 
 if __name__ == "__main__":
-    my_app()
+    main()
