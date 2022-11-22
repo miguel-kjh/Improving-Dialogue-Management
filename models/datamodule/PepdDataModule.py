c9150d44a3bb55a60b59270335c8a4e5f054f632
miguel-kjh <miguelangel.kjh@gmail.com>
2022-11-22 09:12:47 +0000

    update pedp

--- models/datamodule/PepdDataModule.py
+++ models/datamodule/PepdDataModule.py
@@ -22,14 +22,24 @@ class PepdDataset:
         row = self.df.iloc[index]
         state = torch.tensor(row['State'].tolist())
         a_target_gold = torch.tensor(row['Set_Label'].tolist(), dtype=torch.long)
-        a_target_full = copy.deepcopy(a_target_gold)
-        last_pos = torch.tensor([row['Last_position']], dtype=torch.long).T
-        s_pos = copy.deepcopy(last_pos)
+        s_pos = torch.tensor([row['Last_position']], dtype=torch.long).T
         actions = torch.tensor(row['Real_label'], dtype=torch.long)
         try:
-            next_state = torch.tensor(self.df.iloc[index + 1]['State'].tolist())
+            next_state = torch.tensor(
+                self.df.iloc[index + 1]['State'].tolist()
+            )
+            a_target_full = torch.tensor(
+                torch.tensor(
+                    self.df.iloc[index + 1]['Set_Label'].tolist(),
+                    dtype=torch.long
+                )
+            )
+            last_pos = torch.tensor([self.df.iloc[index + 1]['Last_position']], dtype=torch.long).T
         except IndexError:
             next_state = torch.zeros_like(state)
+            a_target_full = torch.zeros_like(a_target_gold)
+            last_pos = torch.zeros_like(s_pos)
+
         return state, next_state, s_pos, a_target_gold, last_pos, a_target_full, actions, index
 
 
